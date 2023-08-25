e32c07e08b5a6f624350675d79711cb2f4c50749
Guiideed <guilhermenovolg@gmail.com>
2023-08-24 23:56:10 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let botao = document.getElementById("btn_teste"); 
+console.log(botao);
+botao.addEventListener("click",
+function(){
+    alert("Você clicou no botão");
+    console.log("Clicou no botão");
+});
