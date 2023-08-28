1955ffb3db111f8478aff5bedb410676016a693f
Guiideed <guilhermenovolg@gmail.com>
2023-08-28 18:58:38 -0300

    finalizando

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -1,7 +1,35 @@
-let botao = document.getElementById("btn_teste"); 
-console.log(botao);
-botao.addEventListener("click",
-function(){
-    alert("Você clicou no botão");
-    console.log("Clicou no botão");
-});
+const offset =0;
+const limit = 10;
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+function convertPokemonToList(pokemon){
+    return `
+    <li class="pokemon">
+                <span class="numero">#001</span>
+                <span class="nome">${pokemon.name}</span>
+                
+                <div class="caracteristicas">
+                    <ol class="tipos">
+                        <li class="tipo">Grama</li>
+                        <li class="tipo">Venenoso</li>
+                    </ol>
+
+                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+                </div>
+            </li>
+            `
+
+}
+const pokemonList = document.getElementById('pokemonList');
+//interface de uma promisse
+fetch(url)
+    .then((response)=>response.json())
+    .then((jsonBody)=>jsonBody.results)
+    .then((pokemons) =>{
+        for (let i = 0; i < pokemons.length; i++) {
+            const pokemon = pokemons[i];
+            pokemonList.innerHTML += convertPokemonToList(pokemon)
+            
+        }
+}).catch((error)=>console.error(error));
+
